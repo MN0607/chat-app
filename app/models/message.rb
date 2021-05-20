66662ac698f19940056e087dced978d6e641f0b0
MN0607 <kobukuro.mun@gmail.com>
2021-05-21 07:22:13 +0900

    .field_with_errors {   display: contents; }

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
 
+  validates :content, presence: true
+
   @message = Message.new
   @room = Room.find(params[:room_id])
 end
